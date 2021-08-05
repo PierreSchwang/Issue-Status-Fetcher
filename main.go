a70a0ee5b0abcbd6c199b55fb3bc6452994d4ad2
Pierre Maurice Schwang <mail@pschwang.eu>
2021-08-05 17:21:55 +0200

    File based logging + don't reuse twitter client Log-Output gets appended to local file, so limitations of console buffer don't erase log output in screen sessions whatsoever Twitter-Client gets reauthorized every status update, to make sure, that api tokens are up-to-date and the api session can be used after a few hours / days of runtime.

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "issue-status-fetcher/cmd"
+import (
+	"io"
+	"issue-status-fetcher/cmd"
+	"log"
+	"os"
+)
 
 func main() {
+	// Add file based logging
+	f, err := os.OpenFile("status.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+			log.Fatal("Failed to close file stream: ", err)
+		}
+	}(f)
+	writer := io.MultiWriter(os.Stdout, f)
+	log.SetOutput(writer)
 	cmd.Run()
 }
